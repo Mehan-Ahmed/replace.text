37845f442be9e3b64d68b1d9d5ec50bc51c5e95f
Developer Tanjed <125651526+Mehan-Ahmed@users.noreply.github.com>
2023-04-10 17:23:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+function replaceText() {
+  // Get the input data, find text, and replace with values
+  const inputData = document.getElementById("input-data").value;
+  const findText = document.getElementById("find-text").value;
+  const replaceWith = document.getElementById("replace-with").value;
 
+  // Replace the text
+  const outputData = inputData.replace(new RegExp(findText, "g"), replaceWith);
+
+  // Display the output
+  const outputElement = document.getElementById("output");
+  outputElement.innerText = outputData;
+  outputElement.style.display = "block";
+}
+
+function copyToClipboard() {
+  // Get the output data
+  const outputData = document.getElementById("output").innerText;
+
+  // Create a temporary textarea element and set its value to the output data
+  const tempTextarea = document.createElement("textarea");
+  tempTextarea.value = outputData;
+
+  // Add the textarea element to the document
+  document.body.appendChild(tempTextarea);
+
+  //
+// Select the text inside the textarea element
+  tempTextarea.select();
+
+  // Copy the selected text to clipboard
+  document.execCommand("copy");
+
+  // Remove the textarea element from the document
+  document.body.removeChild(tempTextarea);
+
+  // Show a message to indicate that the text has been copied to clipboard
+  const copyButton = document.getElementById("copy-button");
+  copyButton.innerText = "Copied!";
+  setTimeout(function() {
+    copyButton.innerText = "Copy to Clipboard";
+  }, 3000);
+}
